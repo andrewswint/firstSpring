cc65e9bd0de85e075d2337a4462ec41015faca72
andrewswint <andrewswint1976@gmail.com>
2020-01-27 10:21:06 -0600

    path variables

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("/deimos/{name}")
+    @GetMapping("/deimos/{days}")
     @ResponseBody
+    public String hello(@PathVariable String days) {
+        return "There are  " + days + " until dev day";
+    }
 
-    public String hello(@PathVariable String name) {
-        return "hello " + name;
+    @GetMapping("/deimos")
+    @ResponseBody
+    public String hello() {
+        return "hello";
     }
 
 }

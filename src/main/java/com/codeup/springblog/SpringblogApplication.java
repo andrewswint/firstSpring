d4c593ac368128ccd4fba7c6acf355eae450fe23
andrewswint <andrewswint1976@gmail.com>
2020-01-31 16:09:25 -0600

    added sout for compile completion

--- src/main/java/com/codeup/springblog/SpringblogApplication.java
+++ src/main/java/com/codeup/springblog/SpringblogApplication.java
@@ -8,6 +8,15 @@ public class SpringblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringblogApplication.class, args);
+
+		System.out.println("               __\n" +
+				"    ..=====.. |==|\n" +
+				"    ||     || |= |\n" +
+				" _  ||     || |^*| _\n" +
+				"|=| o=,===,=o |__||=|\n" +
+				"|_|  _______)~`)  |_|\n" +
+				"    [=======]  ()   ");
 	}
+
 
 }
